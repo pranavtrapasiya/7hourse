adbb1957d7645de9eebc7ad7a26bffa491cc407a
Henil Shingala <henilshingala2462@gmail.com>
2026-06-22 19:56:41 +0530

    Revert staticfiles storage to fix collectstatic crash

--- app/settings.py
+++ app/settings.py
@@ -160,7 +160,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
